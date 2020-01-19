3eacf1cc6d4396fe07607237dbae9b25a38025e1
Shashank Sharma <shashank.sharma@landmarkgroup.in>
2020-01-20 02:34:58 +0530

    added margins between the slides and fixed button initial state issue

--- js/index.js
+++ js/index.js
@@ -4,20 +4,24 @@ var carouselWrapperWidth = document.getElementById('customCarousel')
   .offsetWidth;
 var totalSlides = slideCollection.length;
 var totalWidth = 0;
+var slideWidth;
+var slidesToShow = 4;
+var counter = 0;
+var transformWidth = 0;
 var track = document.getElementById('track');
 var prevBtn = document.getElementById('prev');
 var nextBtn = document.getElementById('next');
 var prevArw = document.getElementById('arw-prv');
 var nextArw = document.getElementById('arw-nxt');
-var slideWidth;
-var slidesToShow = 4;
-var counter = 0;
-var transformWidth = 0;
+document.getElementById('prev').disabled = true;
+var margin = 20;
 
 (Slider = () => {
   /*============================ Calculate the Total Width of the Track ==========================*/
   (CalculateWidth = () => {
-    totalWidth = (carouselWrapperWidth / slidesToShow) * totalSlides;
+    totalWidth =
+      (carouselWrapperWidth / slidesToShow) * totalSlides +
+      margin * totalSlides;
     return totalWidth;
   })();
 
@@ -29,14 +33,25 @@ var transformWidth = 0;
   /*============================== Setting the Width of the Slide ==================================*/
   (SetSlideWidth = () => {
     slideWidth =
-      ((carouselWrapperWidth / slidesToShow) * totalSlides) / totalSlides;
+      ((carouselWrapperWidth / slidesToShow) * totalSlides -
+        margin * totalSlides) /
+      totalSlides;
+
+    var elements = document.getElementsByClassName('c-slide');
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.width = slideWidth + 'px';
+    }
+
+    for (var i = 0; i < elements.length - 1; i++) {
+      elements[i].style.margin = '0' + margin / 2 + 'px';
+    }
   })();
 })();
 
 /*==================================== Handler for Next Button Click ===============================*/
 NextSlideHandler = () => {
   counter++;
-  transformWidth = transformWidth - slideWidth;
+  transformWidth = transformWidth - slideWidth - margin;
   track.style.transform = 'translate3d(' + transformWidth + 'px, 0px, 0px)';
   SetButtonState();
 };
@@ -44,7 +59,7 @@ NextSlideHandler = () => {
 /*==================================== Handler for Previous Button Click =============================*/
 PrevSlideHandler = () => {
   counter--;
-  transformWidth = transformWidth + slideWidth;
+  transformWidth = transformWidth + slideWidth + margin;
   track.style.transform = 'translate3d(' + transformWidth + 'px, 0px, 0px)';
   SetButtonState();
 };
